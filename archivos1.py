4c25056d23eb610e564055113e7dfc07e513b064
estebanlus <83309196+estebanlus@users.noreply.github.com>
2022-11-21 20:07:20 -0300

    Create archivos1.py

---
+++ archivos1.py
@@ -0,0 +1,9 @@
+#Lectura de Archivos
+
+f = open('fichero.txt', 'w')
+
+f.write('Esta es una linea de prueba\n')
+f.write('Esta es una linea de prueba\n')
+f.write('Esta es una linea de prueba\n')
+
+f.close()

85e38b77612feb50bb7722b643665ac5c5d10a99
estebanlus <83309196+estebanlus@users.noreply.github.com>
2022-11-21 17:26:24 -0300

    Add files via upload

---
+++ modulos.py
@@ -0,0 +1,8 @@
+#Módulos en python
+
+import opebasicas
+
+print(opebasicas.suma(2,2))
+print(opebasicas.resta(9,3))
+print(opebasicas.multiplica(3,5))
+print(opebasicas.divide(32,8))
---
+++ opebasicas.py
@@ -0,0 +1,17 @@
+#Operaciones básicas
+
+#Suma
+def suma(a,b):
+    return a+b
+
+#Resta
+def resta(a,b):
+    return a-b
+
+#Multiplicación
+def multiplica(a,b):
+    return a*b
+
+#División
+def divide(a,b):
+    return a/b
